041b045b7bb525a082f72aa293b92faf014ee882
johnglenn30 <jglenn8930@gmail.com>
2023-03-10 20:16:17 +0800

    jobs

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Job
 {
-    public string _company = "Software Engineer";
+    public string _company;
     public string _jobTitle;
     public int _startYear;
     public int _endYear;
