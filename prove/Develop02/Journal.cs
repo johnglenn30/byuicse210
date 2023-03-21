04b220fabff3020d6d73ff64078d547f4ebace87
johnglenn30 <jglenn8930@gmail.com>
2023-03-22 01:39:44 +0800

    w2 prove developer

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -55,6 +55,7 @@ namespace JournalApp
                         {
                             CreateFile();
                         }
+                        
                     }
                     
                 }
